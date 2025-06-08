b974ae3be91ca5775439834a97120ebc2cb54300
jack-undead-dev <jackson@undead-dev.com>
2025-06-08 16:36:07 -0300

    routing corrigido

--- app/index.jsx
+++ app/index.jsx
@@ -2,16 +2,13 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon from '@react-native-vector-icons/material-icons';
 import { GameProvider } from '@/src/context/GameContext';
 import AddGameScreen from '@/src/screens/AddGameScreen';
 import GameDetailScreen from '@/src/screens/GameDetailScreen';
 import GameListScreen from '@/src/screens/GameListScreen';
 import HomeScreen from '@/src/screens/HomeScreen';
 import StatsScreen from '@/src/screens/StatsScreen';
-import { useEffect } from 'react';
-import { Platform } from 'react-native';
-import * as NavigationBar from 'expo-navigation-bar';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -36,34 +33,11 @@ const GameListStack = () => (
   </Stack.Navigator>
 );
 
-const App = () => {
-  
-useEffect(() => {
-  if (Platform.OS === 'android') {
-    const setupNavigationBar = async () => {
-      try {
-        // Tente diferentes cores
-        await NavigationBar.setBackgroundColorAsync('#0f1419'); // Cor de fundo do app
-        await NavigationBar.setButtonStyleAsync('light');
-        
-        // Ou tente com transparência
-        // await NavigationBar.setBackgroundColorAsync('transparent');
-        
-        // Ou força a cor do sistema
-        // await NavigationBar.setBackgroundColorAsync('#000000');
-        
-      } catch (error) {
-        console.log('NavigationBar error:', error);
-      }
-    };
-    
-    setupNavigationBar();
-  }
-}, []);
+const index = () => {
 
   return (
     <GameProvider>
-      <StatusBar barStyle="light-content" backgroundColor="#1a1a2e" translucent={true} />
+      <StatusBar barStyle="light-content" backgroundColor=" #1a1a2e" translucent={true} />
       <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
@@ -101,4 +75,4 @@ useEffect(() => {
   );
 };
 
-export default App;
+export default index;
